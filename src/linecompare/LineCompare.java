8b411d3678778d2f04ea800a0f90b025ec0d18c9
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-30 15:41:44 +0530

    ADD. welcome to the line compare problem

---
+++ src/linecompare/LineCompare.java
@@ -0,0 +1,10 @@
+package linecompare;
+
+public class LineCompare {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To The Line Compare Cmputation Problem");
+		
+	}
+
+}
