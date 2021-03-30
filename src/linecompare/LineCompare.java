5fd08512c143719817cb21efef1f0d24e63c3369
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-03-30 17:14:09 +0530

    ADD.one line is equal, greater or

--- src/linecompare/LineCompare.java
+++ src/linecompare/LineCompare.java
@@ -1,24 +1,16 @@
 package linecompare;
 import java.util.Scanner;
 public class LineCompare {
-	
-	static boolean CheckEquality(double LenthOfLine1 , double LenthOfLine2) {
-		
-		if(LenthOfLine1==LenthOfLine2) 
-			return true;
-		return false;
-	}
-
 
 	public static void main(String[] args) {
 		System.out.println("***Welcome To The Line Compare Computation Problem***");
-		
+
 		Scanner sc = new Scanner(System.in);
 		//Variables for first line
 		int x1 , y1 , x2 , y2 ;
 		double LenthOfLine1, LenthOfLine2;
 		// Points for line 1
-		
+
 		System.out.println("Enter X1 Point");
 		x1 = sc.nextInt();
 		System.out.println("Enter Y1 Point");
@@ -27,11 +19,11 @@ public class LineCompare {
 		x2 = sc.nextInt();
 		System.out.println("Enter Y2 Point");
 		y2 = sc.nextInt();
-	
+
 		//Computation for line 1
 		LenthOfLine1 = Math.sqrt((x2-x1)^2 + (y2-y1));
-		
-		
+
+
 		// Variables for seconds line
 		int x3 , y3, x4 , y4 ;
 		// Points for line 2
@@ -43,20 +35,25 @@ public class LineCompare {
 		x4 = sc.nextInt();
 		System.out.println("Enter Y4 Point");
 		y4 = sc.nextInt();
-		
-		LenthOfLine2 = Math.sqrt((x4-x3)^2 + (y4-y3));
-		
-		System.out.println(" Lenth Of Line 1 is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>" + LenthOfLine1);
-		System.out.println(" Lenth Of Line 2 is : "+"("+x3+","+y3+"),"+"("+x4+","+y4+")===>" + LenthOfLine2);
-		
-		boolean Equals =CheckEquality(LenthOfLine1,LenthOfLine2);
-		if(Equals)
-			System.out.println("Both Lines Are Equals");
-		else
-			System.out.println("Both Lines Are Not Equals");
-		
-	}
-	
-	
 
+		//Computation for line 2
+		LenthOfLine2 = Math.sqrt((x4-x3)^2 + (y4-y3));
+
+		//represents lines co -ordinates
+
+		System.out.println("Lenth Of Line 1 is : "+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>" + LenthOfLine1);
+		System.out.println("Lenth Of Line 2 is : "+"("+x3+","+y3+"),"+"("+x4+","+y4+")===>" + LenthOfLine2);
+
+		// line check
+		
+		if(LenthOfLine1==LenthOfLine2) 
+			System.out.println("Line 1 and Line 2 are equals");
+
+		else if(LenthOfLine1 < LenthOfLine2)
+			System.out.println("Line 1 is less than Line 2");	
+
+		else if(LenthOfLine1 > LenthOfLine2)
+			System.out.println("Line 1 gretter than Line 2");
+
+	}
 }
